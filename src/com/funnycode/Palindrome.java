29653be398f351785886ed79c3b7554564426113
John Martinez <jmartinez5120@gmail.com>
2021-11-16 23:09:29 -0500

    Added Palindrome challenge.

---
+++ src/com/funnycode/Palindrome.java
@@ -0,0 +1,64 @@
+package com.funnycode;
+
+/**
+ * Given an integer x, return true if x is palindrome integer.
+ *
+ * An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+ * <p>
+ * Example 1:
+ * Input: x = 121
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * <p>
+ * Example 3:
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * <p>
+ * Example 4:
+ * Input: x = -101
+ * Output: false
+ *
+ * Constraints:
+ * -231 <= x <= 231 - 1
+ * <p>
+ * Result -> Success
+ * <p>
+ * Runtime: 7 ms, faster than 75.35% of Java online submissions for Palindrome Number.
+ * Memory Usage: 38.2 MB, less than 92.37% of Java online submissions for Palindrome Number.
+ *
+ * <p>
+ *     URL: https://leetcode.com/problems/palindrome-number/
+ */
+public class Palindrome {
+
+    public boolean isPalindrome(int x) {
+        boolean result = true;
+        // Convert the integer to String to get each number and compare it.
+        String inputNumber = Integer.toString(x);
+        for (int i = 0; i < inputNumber.length(); i++){
+            // TODO: this can be improved... I don't need a for loop here, I can take the value a compare it directly.
+            for (int j = inputNumber.length() - i - 1; j > 0; j--) {
+                if (inputNumber.charAt(i) != inputNumber.charAt(j)) {
+                    return false;
+                }
+                break;
+            }
+        }
+
+        return result;
+    }
+}
+
+class PalindromeMain {
+
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        boolean result = palindrome.isPalindrome(123454321);
+        System.out.println("Result: " + result);
+    }
+}
